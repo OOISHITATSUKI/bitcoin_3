8bf9b83d8477424180a4d56e6c528135d5b81a13
“OOISHITATSUKI” <ooisidegesu@gmail.com>
2025-03-31 17:09:11 +0900

    refactor: システム制御ボタンをヘッダーに一本化し、SystemControlsを状態表示専用に変更

--- src/components/SystemControls.jsx
+++ src/components/SystemControls.jsx
@@ -1,42 +1,39 @@
 import React from 'react';
-import { Button, Card, Space, Typography } from 'antd';
-import { PlayCircleOutlined, StopOutlined } from '@ant-design/icons';
+import { Card, Space, Typography, Tag } from 'antd';
 import { useSystem } from '../context/SystemContext';
 import { useApi } from '../context/ApiContext';
 
 const { Text } = Typography;
 
 const SystemControls = () => {
-  const { isRunning, status, startSystem, stopSystem } = useSystem();
+  const { isRunning, lastSyncTime, formatTimeSince } = useSystem();
   const { isConnected } = useApi();
 
   return (
-    <Card title="システム制御" style={{ marginBottom: 16 }}>
+    <Card title="システム状態" style={{ marginBottom: 16 }}>
       <Space direction="vertical" style={{ width: '100%' }}>
         <div style={{ marginBottom: 16 }}>
-          <Text>ステータス: </Text>
-          <Text strong>{status}</Text>
+          <Space>
+            <Text>稼働状態:</Text>
+            <Tag color={isRunning ? 'success' : 'default'}>
+              {isRunning ? '稼働中' : '停止中'}
+            </Tag>
+          </Space>
         </div>
         
-        <Space>
-          <Button
-            type="primary"
-            icon={<PlayCircleOutlined />}
-            onClick={startSystem}
-            disabled={!isConnected || isRunning}
-          >
-            システム起動
-          </Button>
-          
-          <Button
-            danger
-            icon={<StopOutlined />}
-            onClick={stopSystem}
-            disabled={!isRunning}
-          >
-            システム停止
-          </Button>
-        </Space>
+        <div>
+          <Text>API接続:</Text>
+          <Tag color={isConnected ? 'success' : 'error'} style={{ marginLeft: 8 }}>
+            {isConnected ? '接続済み' : '未接続'}
+          </Tag>
+        </div>
+
+        {lastSyncTime && (
+          <div>
+            <Text>最終同期:</Text>
+            <Text style={{ marginLeft: 8 }}>{formatTimeSince(lastSyncTime)}</Text>
+          </div>
+        )}
       </Space>
     </Card>
   );
